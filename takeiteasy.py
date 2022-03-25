fe2076489267bb5509cd8728fe0085edb115e609
Angad-C <72822389+Angad-C@users.noreply.github.com>
2022-03-25 10:30:47 -0700

    Update takeiteasy.py

--- takeiteasy.py
+++ takeiteasy.py
@@ -10,15 +10,28 @@ my_mic = s_r.Microphone(device_index=0)
 
 
 
-story = """Will Wysoglad, a high school senior in Illinois, went viral after posting video diaries documenting how he was repeatedly sent home because he went to school mask-less. 
+story = """
+"Will you walk a little faster?" said a whiting to a snail,
+"There's a porpoise close behind us, and he's treading on my tail.
+See how eagerly the lobsters and the turtles all advance!
+They are waiting on the shingle—will you come and join the dance?
+    Will you, won't you, will you, won't you, will you join the dance?
+    Will you, won't you, will you, won't you, won't you join the dance?
 
-An Illinois judge had recently ruled schools couldn't require masks.
+"You can really have no notion how delightful it will be
+When they take us up and throw us, with the lobsters, out to sea!"
+But the snail replied "Too far, too far!" and gave a look askance—
+Said he thanked the whiting kindly, but he would not join the dance.
+    Would not, could not, would not, could not, would not join the dance.
+    Would not, could not, would not, could not, could not join the dance.
 
-But while two-thirds of American schools have dropped mask requirements, there are students fighting to keep those requirements in place over public health concerns. That includes Leif Aucoin, a theatre major sophomore in Nevada.
-
-We asked both students why they were protesting, and how mask requirement changes in schools were affecting them. 
-
-Produced and edited by Man Sum Lai"""
+"What matters it how far we go?" his scaly friend replied,
+"There is another shore, you know, upon the other side.
+The further off from England the nearer is to France—
+Then turn not pale, beloved snail, but come and join the dance.
+    Will you, won't you, will you, won't you, will you join the dance?
+    Will you, won't you, will you, won't you, won't you join the dance?"
+"""
 
 print("""
 Welcome to Take it Easy!
